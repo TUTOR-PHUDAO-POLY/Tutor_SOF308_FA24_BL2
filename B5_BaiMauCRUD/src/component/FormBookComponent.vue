46bd963a6908ee1ba5a2aa56f56372bd48bf9d75
HangNT169 <HangNT169@fpt.edu.vn>
2024-11-24 16:55:44 +0700

    Buổi 6: Finish CRUD

--- B5_BaiMauCRUD/src/component/FormBookComponent.vue
+++ B5_BaiMauCRUD/src/component/FormBookComponent.vue
@@ -1,17 +1,27 @@
 <template>
   <section>
-    Tên: <input />
+    Tên: <input type="text" v-model="book.ten" />
     <br />
     Loại:
-    <select>
-      <option>Loai 1</option>
-      <option>Loai 2</option>
-      <option>Loai 3</option>
-      <option>Loai 4</option>
+    <select v-model="book.loai">
+      <option value="Loại 1">Loai 1</option>
+      <option value="Loại 2">Loai 2</option>
+      <option value="Loại 3">Loai 3</option>
+      <option value="Loại 4">Loai 4</option>
     </select>
     <br />
-    Tác giả: <input />
+    Tác giả: <input type="text" v-model="book.tacGia" />
     <br />
-    Giá: <input />
+    Giá: <input type="text" v-model="book.gia" />
   </section>
 </template>
+<script setup>
+defineModel('book', {
+  default: () => ({
+    ten: '',
+    loai: '',
+    tacGia: '',
+    gia: '',
+  }),
+})
+</script>
